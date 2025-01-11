33c1f1814aab76d7f33e72238316ec3c2defa1bb
SunJin_Yoon <sunjin.yoon@fnsusa.com>
2025-01-11 13:48:49 -0800

    Init SpringMvc Part2

---
+++ src/main/java/hello/spirngmvc/SpirngmvcApplication.java
@@ -0,0 +1,13 @@
+package hello.spirngmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpirngmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/spirngmvc/SpirngmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.spirngmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
